29f65680510d527d37fde54d3e0fde9b8ce3ce07
j <dirkmcpherson@gmail.com>
2026-02-11 16:53:02 -0500

    random seed / runname in training bash script

--- modemv2/scripts/maniskill/modemv2_ms_demos.sh
+++ modemv2/scripts/maniskill/modemv2_ms_demos.sh
@@ -3,7 +3,8 @@
 # Default values
 MULTI=""
 NAME="exp_name=ms_pick_cube_demos"
-SEED="seed=42069"
+SEED_VAL=$((1 + RANDOM % 1000))
+SEED="seed=$SEED_VAL"
 DEMOS="demos=200"
 LAUNCHER="hydra/launcher=local"
 BATCH_SIZE="batch_size=256"
